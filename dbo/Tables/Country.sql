c88511a8f172dab8f35f14836abb3a1af78ab92e
Ravesh Kumar <ravesh.k@orioninc.com>
2022-02-28 00:54:48 +0530

    Update db column

--- dbo/Tables/Country.sql
+++ dbo/Tables/Country.sql
@@ -2,5 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NOT NULL, 
-    [Code] VARCHAR(50) NOT NULL
+    [Code] VARCHAR(50) NOT NULL, 
+    [CreatedBy] INT NULL, 
+    [CreatedDate] DATETIME NULL
 )
